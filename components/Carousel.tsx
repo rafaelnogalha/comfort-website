9e746ee6085c1b1aa4a03b30c7f62635ad47e30b
rafaelnogalha <rafaelnogalha@gmail.com>
2022-03-01 21:26:16 -0300

    updated v0001

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -60,16 +60,15 @@ export default function Carousel() {
       imageSrc:'/project_pic_2.jpg',
       title: 'FILHOS DA LIBERDADE',
       description: 
-      'O projeto busca mudar radicalmente' 
-      + ' a vida de crianças forçadas a' 
-      + ' viver nas comunidades de Ruanda'  
-      + ' e função da pobreza ruptura'
-      + ' familiar ou orfandade. O programa'
-      + ' fornece comida, educação, cuidados'
-      + ' com a saúde, esportes, música e um'
-      + ' ambiente de acolhimento e amor.'
-      + ' Buscamos reintegrar os laços'
-      + ' familiares, quando possível.',
+      'Os padrinhos deste programa apoiam' 
+      + ' crianças e jovens que foram recrutados' 
+      + ' pelas milícias no Congo ou para'  
+      + ' guerrearem e roubar ou, no caso das'
+      + ' meninas, para serem escravas sexuais.'
+      + ' Eles recebem comida, educação,'
+      + ' um lugar para morar, cuidados com'
+      + ' a saúde e um ambiente que os'
+      + ' motiva a progredir' 
     }
   ]
 
