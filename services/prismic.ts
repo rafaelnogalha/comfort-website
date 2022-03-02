7323350da0ad17b63345beebf3f832c52e7bd67e
rafaelnogalha <rafaelnogalha@gmail.com>
2022-03-01 21:31:33 -0300

    updated v001

--- services/prismic.ts
+++ services/prismic.ts
@@ -1,12 +1,12 @@
-// import Prismic from '@prismicio/client'
+import Prismic from '@prismicio/client'
 
-// export function getPrismicClient() {
-//   const prismic = Prismic.client(
-//     process.env.PRISMIC_ACCESS_TOKEN,
-//     {
-//       accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-//     }
-//   )
+export function getPrismicClient() {
+  const prismic = Prismic.client(
+    process.env.PRISMIC_ACCESS_TOKEN,
+    {
+      accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    }
+  )
 
-//   return prismic;
-// }
+  return prismic;
+}
